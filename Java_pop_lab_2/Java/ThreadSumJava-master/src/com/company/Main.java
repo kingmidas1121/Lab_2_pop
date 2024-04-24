aec802b7e5cc62d8648c725cee483c640732e748
kingmidas1121 <95761121+kingmidas1121@users.noreply.github.com>
2024-04-24 05:28:23 +0300

    .

--- Java_pop_lab_2/Java/ThreadSumJava-master/src/com/company/Main.java
+++ Java_pop_lab_2/Java/ThreadSumJava-master/src/com/company/Main.java
@@ -3,7 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        new ControleThread(0,8,10000,
-                0,0).EntryPoint();
+        new ControleThread(0,8,10000,0,0).EntryPoint();
     }
 }
